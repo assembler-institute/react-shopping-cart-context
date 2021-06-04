8b2c9a2c04fbbf1c783a38d7c963f22afd693324
orionalfa <orionalfacentauri@gmail.com>
2021-06-04 13:41:51 +0200

    feat: Checkout button working and Nav button added

--- src/components/CheckoutFooter/CheckoutFooter.js
+++ src/components/CheckoutFooter/CheckoutFooter.js
@@ -1,12 +1,26 @@
 import React from "react";
-// import { NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
+import Button from "../Button";
 import "./CheckoutFooter.scss";
 
 function CheckoutFooter() {
+  const [step, setStep] = useState(1);
+
   return (
     <header className="bg-primary mb-4">
       <h1>CheckoutFooter</h1>
+      <NavLink to={`/Checkout/step-${step}`}>
+        <Button
+          onClick={() => {
+            setStep(step + 1);
+            console.log(step);
+          }}
+        >
+          Next
+        </Button>
+      </NavLink>
     </header>
   );
 }
--- src/hoc/withCheckout.js
+++ src/hoc/withCheckout.js
@@ -1,8 +1,8 @@
 import React from "react";
-import CheckoutFooter from "../components/CheckoutFooter/CheckoutFooter";
-import CheckoutForm from "../components/CheckoutForm/CheckoutForm";
-import CheckoutHeader from "../components/CheckoutHeader/CheckoutHeader";
-import CheckoutSideBar from "../components/CheckoutSideBar/CheckoutSideBar";
+import CheckoutFooter from "../components/CheckoutFooter";
+import CheckoutForm from "../components/CheckoutForm";
+import CheckoutHeader from "../components/CheckoutHeader";
+import CheckoutSideBar from "../components/CheckoutSideBar";
 
 // function getDisplayName(WrappedComponent) {
 //   return WrappedComponent.displayName || WrappedComponent.name || "Component";
