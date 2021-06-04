903e4c45baee0779e057a353674cff7db59fe80b
Hayk Petrosyan <hpetrosyan.projects@gmail.com>
2021-06-04 17:54:04 +0200

    feat: Shipping form created

---
+++ src/components/ShippingForm/shipping-schema.js
@@ -0,0 +1,12 @@
+import * as Yup from "yup";
+
+const ShippingSchema = Yup.object().shape({
+  addressOne: Yup.string().max(50).required("Required"),
+  addressTwo: Yup.string().max(50),
+  zipCode: Yup.string().min(5).max(5).required("Required"),
+  country: Yup.string().min(5).max(20).required("Required"),
+  state: Yup.string().required("Required"),
+  city: Yup.string().required("Required"),
+});
+
+export default ShippingSchema;
--- src/pages/Shipping/Shipping.js
+++ src/pages/Shipping/Shipping.js
@@ -1,7 +1,32 @@
 import React from "react";
+import ShippingForm from "../../components/ShippingForm/ShippingForm";
+import withLayout from "../../hoc/withLayout";
 
 function Shipping() {
-  return <h1>Shipping</h1>;
+  return (
+    <>
+      <h1>Shipping Information</h1>
+      <div className="row">
+        <div className="col col-6">
+          <header className="jumbotron">
+            <h3>Your details</h3>
+
+            <hr />
+
+            <ShippingForm />
+          </header>
+        </div>
+
+        <div className="col col-6">
+          <header className="jumbotron">
+            <h3>Your Cart</h3>
+
+            <hr />
+          </header>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Shipping;
+export default withLayout(Shipping);
