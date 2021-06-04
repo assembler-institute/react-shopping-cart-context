a4fb2e26b933c84490bf6a83a24a58b78bfc10c7
Jonathan Cedeño <jony.cede@gmail.com>
2021-06-04 13:15:17 +0200

    feat: structure of form data is implemented fixes #5

--- src/pages/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout.js
@@ -4,6 +4,31 @@ import CheckoutProfile from "./CheckoutProfile";
 
 import { PROFILE } from "../../utils/constants";
 
+const initialValueFormData = {
+  name: "",
+  email: "",
+  phone: "",
+  address: "",
+  city: "",
+  postCode: "",
+  country: "",
+  card: {
+    cardName: "",
+    cardNumber: "",
+    cardDate: "",
+    cardCVV: "",
+    cardTerms: false,
+  },
+  paypal: {
+    paypalUser: "",
+    paypalPassword: "",
+  },
+  applePay: {
+    applePayUser: "",
+    applePayPassword: "",
+  },
+};
+
 function Checkout({ processStep }) {
   return <>{processStep === PROFILE && <CheckoutProfile />}</>;
 }
