5796ecb5b835b354436452ce0e27358860693c26
Mathias Fortuna <mathiasfortuna@hotmail.com>
2021-06-04 16:10:59 +0200

    Added payment routing

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -2,3 +2,4 @@ export const HOME = "/";
 export const NEWPRODUCT = "/new-product";
 export const DETAIL = "/checkout/step-1";
 export const ADDRESS = "/checkout/step-2";
+export const PAYMENT = "/checkout/step-3";
