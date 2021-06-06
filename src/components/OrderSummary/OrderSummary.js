b63536a638e5a419c19d8bd428702a4b1ee23e49
Marc <marcsr03@gmail.com>
2021-06-06 21:39:57 +0200

    feature: order summary sidebar --> first version

--- src/components/OrderSummary/OrderSummary.js
+++ src/components/OrderSummary/OrderSummary.js
@@ -16,26 +16,28 @@ function OrderSummary({ cartItems, handleRemove, handleChange, ...props }) {
           <h2 className="h3 mt-2">Order Summary</h2>
           <hr className="mb-3" />
         </div>
+        <div className="cartItemsContainer">
+          {cartItems.length > 0 ? (
+            cartItems.map((item) => (
+              <ShoppingCartItem
+                key={item.id}
+                id={item.id}
+                title={item.title}
+                price={item.price}
+                img={item.img}
+                quantity={item.quantity}
+                unitsInStock={item.unitsInStock}
+                handleRemove={handleRemove}
+                handleChange={handleChange}
+              />
+            ))
+          ) : (
+            <div className="col mb-4">
+              <h4>Your cart is empty</h4>
+            </div>
+          )}
+        </div>
 
-        {cartItems.length > 0 ? (
-          cartItems.map((item) => (
-            <ShoppingCartItem
-              key={item.id}
-              id={item.id}
-              title={item.title}
-              price={item.price}
-              img={item.img}
-              quantity={item.quantity}
-              unitsInStock={item.unitsInStock}
-              handleRemove={handleRemove}
-              handleChange={handleChange}
-            />
-          ))
-        ) : (
-          <div className="col mb-4">
-            <h4>Your cart is empty</h4>
-          </div>
-        )}
         <div className="col shopping__cart__footer">
           <div className="row row-cols-1 flex-column">
             <div className="col">
@@ -48,6 +50,21 @@ function OrderSummary({ cartItems, handleRemove, handleChange, ...props }) {
               <hr />
             </div>
           </div>
+        </div>
+        <div className="col">
+          <div className=" mb-4 gift__CardContainer">
+            <p className="ml-4">Gift card/Discount code</p>
+            <div className="gift__Form">
+              <div className="gift__mockInput">hello</div>
+              <button
+                type="button"
+                className="btn btn-outline-primary gift__Btn"
+              >
+                Apply
+              </button>
+            </div>
+          </div>
+          <hr />
         </div>
       </div>
     </aside>
