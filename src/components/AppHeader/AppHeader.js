2d53dcb700d0c93da9fce12d1f80e896bfa0cca7
JordixArnau <75805206+JordixArnau@users.noreply.github.com>
2021-06-07 13:46:08 +0200

    style: added logo and hidden-item conditional class (#16)

--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -1,9 +1,16 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
+import logo from "../../assets/img/zapato_veloz_logo.png";
 
 import "./AppHeader.scss";
 
 function AppHeader({ ...props }) {
+  let checkout = true;
+  const location = useLocation();
+  if (location.pathname === "/" || location.pathname === "/new-product") {
+    checkout = false;
+  }
+
   return (
     <header className="bg-primary mb-4" {...props}>
       <div className="container-fluid">
@@ -15,11 +22,11 @@ function AppHeader({ ...props }) {
               className="navbar-brand"
               to="/"
             >
-              Home
+              <img src={logo} alt="Zapato Veloz" />
             </NavLink>
 
             <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
+              <li className={`${checkout ? "hidden-item" : "nav-item"}`}>
                 <NavLink
                   exact
                   activeClassName="active"
