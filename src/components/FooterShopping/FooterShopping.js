8fc6fd8fc3eb87dfbb7dc084105eb024dd9bc5e5
Almego <alvaromerinogar@gmail.com>
2021-06-07 16:54:24 +0200

    footer finish

--- src/components/FooterShopping/FooterShopping.js
+++ src/components/FooterShopping/FooterShopping.js
@@ -4,7 +4,18 @@ import React from "react";
 import "./FooterShopping.scss";
 
 function FooterShopping() {
-  return <footer className="footer_Shopping">hola</footer>;
+  return (
+    <footer className="footer_Shopping">
+      <div>
+        <span> © 2021 Shopping Wizard.</span>
+      </div>
+      <div className="fotter-right">
+        <div>Policy & Cookies</div>
+        <div> &nbsp; &nbsp;</div>
+        <div> Tema & Coditions</div>
+      </div>
+    </footer>
+  );
 }
 
 export default FooterShopping;
