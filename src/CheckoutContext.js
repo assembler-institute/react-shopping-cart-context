767e21d732da28d449c46a2ca2d73b6752ad624b
JordixArnau <75805206+JordixArnau@users.noreply.github.com>
2021-06-07 09:04:23 +0200

    Feat/11 define app state (#14)
    * feat: define basic structure for the App state
    
    * refactor!: changed useState to Context

---
+++ src/CheckoutContext.js
@@ -0,0 +1,63 @@
+import React, { createContext } from "react";
+
+const orderContext = createContext({
+  stepOne: {
+    completed: false,
+    name: "",
+    email: "",
+    country: "",
+    phone: "",
+  },
+  stepTwo: {
+    completed: false,
+    address: "",
+    city: "",
+    zip: "",
+    country: "",
+  },
+  stepThree: {
+    completed: false,
+    payMethod: "",
+    cardHolder: "",
+    cardNumber: "",
+    expiryDate: "",
+    cvv: "",
+    acceptedTerms: false,
+  },
+});
+
+function CheckoutContext({ children }) {
+  return (
+    <orderContext.Provider
+      value={{
+        stepOne: {
+          completed: false,
+          name: "",
+          email: "",
+          country: "",
+          phone: "",
+        },
+        stepTwo: {
+          completed: false,
+          address: "",
+          city: "",
+          zip: "",
+          country: "",
+        },
+        stepThree: {
+          completed: false,
+          payMethod: "",
+          cardHolder: "",
+          cardNumber: "",
+          expiryDate: "",
+          cvv: "",
+          acceptedTerms: false,
+        },
+      }}
+    >
+      {children}
+    </orderContext.Provider>
+  );
+}
+
+export default CheckoutContext;
