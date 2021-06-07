c2c5675eda7417dbe88c60d333be1fd38d146d37
Almego <alvaromerinogar@gmail.com>
2021-06-07 17:04:37 +0200

    steps config

--- src/pages/Checkout/Step2.js
+++ src/pages/Checkout/Step2.js
@@ -1,16 +1,24 @@
 import React from "react";
+
 import ShoppingLayout from "../../hoc/ShoppingLayout";
 import ProgressBar from "../../components/ProgressBar";
+import OrderSummary from "../../components/OrderSummary";
+import FormShopping from "../../components/FormShopping";
 
-function Step2({ ...routeProps }) {
+function Step2({ cartItems, handleRemove, handleChange, ...routeProps }) {
   return (
-    <div className="step1_main--container">
-      <div>
+    <div className="step1__main--container">
+      <div className="step1__main--container--form">
         <ProgressBar {...routeProps} />
-        <div>Form payment</div>
-        <div>Payment Button</div>
+        <FormShopping />
       </div>
-      <div>Resum</div>
+      <div className="col">
+        <OrderSummary
+          cartItems={cartItems}
+          handleRemove={handleRemove}
+          handleChange={handleChange}
+        />
+      </div>
     </div>
   );
 }
