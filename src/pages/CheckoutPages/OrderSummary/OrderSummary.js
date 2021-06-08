efd1080bb5fa40b27fd72e99e572daf9209b60d5
Miguel Perez Martinez <migueldw2.0@gmail.com>
2021-06-08 12:47:11 +0200

    first commit fixes #17

--- src/pages/CheckoutPages/OrderSummary/OrderSummary.js
+++ src/pages/CheckoutPages/OrderSummary/OrderSummary.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
 import withLayout from "../../../hoc/withLayout";
+
+import checkoutContext from "../../../context/checkoutData";
 
 const isCheckout = true;
 
 function OrderSummary() {
+  const { state } = useContext(checkoutContext);
+  console.log(state);
   return (
     <div className="row">
-      <div className="col col-12">Order summary page</div>
+      <div className="col col-12">
+        <div className="row">
+          <div className="col col-4">
+            <h3>Personal data</h3>
+            <ul>
+              <li>{state.name}</li>
+            </ul>
+          </div>
+          <div className="col col-4">Billing address </div>
+          <div className="col col-4">Payment method </div>
+        </div>
+        <div className="row">
+          <div className="col col-12">Pursache resume</div>
+        </div>
+      </div>
     </div>
   );
 }
