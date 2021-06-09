ae39a9afde4c7ee42d2e092a38eaabfc66841644
Erick Noiztbander <aventum2@gmail.com>
2021-06-09 14:14:36 +0200

    ContextProvider SemiArreglado

--- src/components/FormShopping/formSchemaShopping.js
+++ src/components/FormShopping/formSchemaShopping.js
@@ -6,6 +6,15 @@ const formSchemaShopping = Yup.object().shape({
     .max(25, "The title is too long!")
     .required("The title is required"),
   email: Yup.string().email("Invalid email").required("Required"),
+  // cardNumber: Yup.string().label("Card number").min(16).max(16).required(16),
+  // cardCVVCode: Yup.string().label("CVC").min(3).max(4).required(3),
+  // cardHolderName: Yup.string().label("Name on card").max(20).min(2).required(),
+  // expiryMonth: Yup.string().label("Expiry month").min(2).max(2).required(),
+  // cardExpirationDate: Yup.string()
+  //   .label("Expiry year")
+  //   .min(4)
+  //   .max(4)
+  //   .required(),
 });
 
 export default formSchemaShopping;
