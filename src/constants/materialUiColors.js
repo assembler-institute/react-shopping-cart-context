d7c28f3b602bb3158040865720ab0388bf0cf967
Mathias Fortuna <mathiasfortuna@hotmail.com>
2021-06-10 13:19:35 +0200

    Set secondary color to brand secondary color

--- src/constants/materialUiColors.js
+++ src/constants/materialUiColors.js
@@ -10,7 +10,7 @@ export const theme = createMuiTheme({
     },
     secondary: {
       light: "#0066ff",
-      main: "#0066ff",
+      main: "#212529",
       // dark: will be calculated from palette.secondary.main,
       contrastText: "#ffcc00",
     },
