40da7ba392f234dfee0cdfb42959654617e46684
Ricard Garcia <imricardgarcia@gmail.com>
2021-06-10 16:49:58 +0200

    style: deleted commented code blocks
    Fixes #23

--- src/App.js
+++ src/App.js
@@ -27,36 +27,14 @@ import {
   NEWPROD_URL,
 } from "./utils/constants";
 
-// function buildNewCartItem(cartItem) {
-//   if (cartItem.quantity >= cartItem.unitsInStock) {
-//     return cartItem;
-//   }
-
-//   return {
-//     id: cartItem.id,
-//     title: cartItem.title,
-//     img: cartItem.img,
-//     price: cartItem.price,
-//     unitsInStock: cartItem.unitsInStock,
-//     createdAt: cartItem.createdAt,
-//     updatedAt: cartItem.updatedAt,
-//     quantity: cartItem.quantity + 1,
-//   };
-// }
-
 const PRODUCTS_LOCAL_STORAGE_KEY = "react-sc-state-products";
-// const CART_ITEMS_LOCAL_STORAGE_KEY = "react-sc-state-cart-items";
 
 function App() {
   const [products, setProducts] = useState(() =>
     loadLocalStorageItems(PRODUCTS_LOCAL_STORAGE_KEY, []),
   );
-  // const [cartItems, setCartItems] = useState(() =>
-  //   loadLocalStorageItems(CART_ITEMS_LOCAL_STORAGE_KEY, []),
-  // );
 
   useLocalStorage(products, PRODUCTS_LOCAL_STORAGE_KEY);
-  // useLocalStorage(cartItems, CART_ITEMS_LOCAL_STORAGE_KEY);
 
   const [isLoading, setIsLoading] = useState(false);
   const [hasError, setHasError] = useState(false);
@@ -79,55 +57,6 @@ function App() {
         });
     }
   }, []);
-
-  // function handleAddToCart(productId) {
-  //   const prevCartItem = cartItems.find((item) => item.id === productId);
-  //   const foundProduct = products.find((product) => product.id === productId);
-
-  //   if (prevCartItem) {
-  //     const updatedCartItems = cartItems.map((item) => {
-  //       if (item.id !== productId) {
-  //         return item;
-  //       }
-
-  //       if (item.quantity >= item.unitsInStock) {
-  //         return item;
-  //       }
-
-  //       return {
-  //         ...item,
-  //         quantity: item.quantity + 1,
-  //       };
-  //     });
-
-  //     setCartItems(updatedCartItems);
-  //     return;
-  //   }
-
-  //   const updatedProduct = buildNewCartItem(foundProduct);
-  //   setCartItems((prevState) => [...prevState, updatedProduct]);
-  // }
-
-  // function handleChange(event, productId) {
-  //   const updatedCartItems = cartItems.map((item) => {
-  //     if (item.id === productId && item.quantity <= item.unitsInStock) {
-  //       return {
-  //         ...item,
-  //         quantity: Number(event.target.value),
-  //       };
-  //     }
-
-  //     return item;
-  //   });
-
-  //   setCartItems(updatedCartItems);
-  // }
-
-  // function handleRemove(productId) {
-  //   const updatedCartItems = cartItems.filter((item) => item.id !== productId);
-
-  //   setCartItems(updatedCartItems);
-  // }
 
   function handleDownVote(productId) {
     const updatedProducts = products.map((product) => {
@@ -203,40 +132,16 @@ function App() {
         <BrowserRouter>
           <Switch>
             <Route path={PROFILE_URL}>
-              <Checkout
-                fullWidth
-                processStep={PROFILE}
-                // cartItems={cartItems}
-                // handleRemove={handleRemove}
-                // handleChange={handleChange}
-              />
+              <Checkout fullWidth processStep={PROFILE} />
             </Route>
             <Route path={BILLING_URL}>
-              <Checkout
-                fullWidth
-                processStep={BILLING}
-                // cartItems={cartItems}
-                // handleRemove={handleRemove}
-                // handleChange={handleChange}
-              />
+              <Checkout fullWidth processStep={BILLING} />
             </Route>
             <Route path={PAYMENT_URL}>
-              <Checkout
-                fullWidth
-                processStep={PAYMENT}
-                // cartItems={cartItems}
-                // handleRemove={handleRemove}
-                // handleChange={handleChange}
-              />
+              <Checkout fullWidth processStep={PAYMENT} />
             </Route>
             <Route path={SUMMARY_URL}>
-              <Checkout
-                fullWidth
-                processStep={SUMMARY}
-                // cartItems={cartItems}
-                // handleRemove={handleRemove}
-                // handleChange={handleChange}
-              />
+              <Checkout fullWidth processStep={SUMMARY} />
             </Route>
             <Route path={NEWPROD_URL}>
               <NewProduct saveNewProduct={saveNewProduct} />
@@ -244,7 +149,6 @@ function App() {
             <Route path={HOME_URL} exact>
               <Home
                 fullWidth
-                // cartItems={cartItems}
                 products={products}
                 isLoading={isLoading}
                 hasError={hasError}
@@ -253,9 +157,6 @@ function App() {
                 handleUpVote={handleUpVote}
                 handleSetFavorite={handleSetFavorite}
                 showNewProductForm
-                // handleAddToCart={handleAddToCart}
-                // handleRemove={handleRemove}
-                // handleChange={handleChange}
               />
             </Route>
           </Switch>
