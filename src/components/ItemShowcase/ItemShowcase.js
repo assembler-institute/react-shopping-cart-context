ea30534867f4e899b8f7d9a028f5ed9a0cbf6e15
JordixArnau <75805206+JordixArnau@users.noreply.github.com>
2021-06-10 13:32:22 +0200

    feat: create overview sidebar component (#19)
    * feat: define basic structure for the App state
    
    * refactor!: changed useState to Context
    
    * fix: both elements in Context should be exported
    
    * feat: created structure and main features of the Checkout Sidebar
    
    * feat: added dynamic rendering for steps information
    
    * feat: functionality changed due to a new approach

---
+++ src/components/ItemShowcase/ItemShowcase.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+import "./ItemShowcase.scss";
+
+function ItemShowcase({ title, price, img, quantity }) {
+  return (
+    <div className="col">
+      <div className="row flex-column">
+        <div className="col">
+          <div className="row">
+            <div className="col-12 col-xl-4 mb-3 mb-xl-0">
+              <img className="ItemShowcase__img" src={img} alt={title} />
+            </div>
+            <div className="col-12 col-xl-8">
+              <div className="row flex-column mb-3">
+                <div className="col">
+                  <h4 className="h5">
+                    <strong>{title}</strong>
+                  </h4>
+                </div>
+                <div className="col">
+                  <div className="row flex-column">
+                    <span className="col">
+                      <strong>Quantity: </strong>
+                      {quantity}
+                    </span>
+                    <span className="col">
+                      <strong>Price: </strong>
+                      {price}€
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/*         <div className="col">
+          <hr />
+        </div> */}
+      </div>
+    </div>
+  );
+}
+
+export default ItemShowcase;
