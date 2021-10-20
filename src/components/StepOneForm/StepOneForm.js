31b1c1df8acc0ac66c70315be05537cf1e17e387
andarbech <andarbech1.com>
2021-10-20 22:09:27 +0200

    formik

--- src/components/StepOneForm/StepOneForm.js
+++ src/components/StepOneForm/StepOneForm.js
@@ -1,10 +1,51 @@
 import React from "react";
+import Input from "../Input";
 
-function StepOneForm(){
+
+function StepOneForm({
+    type = "text",
+    label = "input-01",
+    id = "input-01",
+    value = "fdgdfgfdg",
+    placeholder = "",
+    handleChange = () => {},
+    handleBlur = () => {},
+    errorMessage,
+    hasErrorMessage,
+    ...props
+  }){
+
+
 return(
     <>
     <div>
-     <h2>helooooo</h2>
+    <form 
+     id="stepOne">
+      <Input
+        type="text"
+        label="Name"
+        id="name"
+        value={value}
+        placeholder="Your name"
+     handleChange={handleChange}
+       />
+      <Input
+        type="text"
+        label="Email"
+        id="email"
+      />
+
+      <span>Phone number</span>
+        <label htmlFor="phone">
+          <input
+            id="phone"
+            name="phone"
+            type="text"
+            placeholder="Your phone number"
+            className="phone"
+          />
+        </label>
+    </form>
     </div>
     </>
 )
---
+++ src/context/userContext/userContex.js
@@ -0,0 +1,14 @@
+import React, { createContext, useReducer } from "react";
+
+
+const checkInitialState = {
+  stepOne: {
+    completed: false,
+    name: "",
+    email: "",
+    countryCode: "+34",
+    phone: "",
+  }
+}
+
+
