b40d0a0d2e49021b66fb5626f9402bd19e41cf1e
Cherrerotinoco <c.herrerotinoco@gmail.com>
2021-10-20 20:32:51 +0200

    Modify context component

--- src/context/eCommerceContext.js
+++ src/context/eCommerceContext.js
@@ -1,14 +1,30 @@
 import { createContext } from "react";
 import { dispatch, state } from "../store/EcommerceReducer";
+import { actionTypes } from "../store/EcommerceReducer";
 
 const eCommerceContext = createContext();
 
 export default function EcommerceContextProvider({children}) {
+
+  const value = {
+    handleDownVote: (id) => dispatch({type: actionTypes.HANDLER_DOWN_VOTE, payload: id}),
+    handleUpVote: (id) => dispatch({type: actionTypes.HANDLER_UP_VOTE, payload: id}),
+    handleSetFavorite: (id) => dispatch({type: actionTypes.HANDLER_SET_FAVORITE, payload: id}),
+    handleAddToCart: (id) => dispatch({type: actionTypes.HANDLER_ADD_TO_CART, payload: id}),
+    handleRemove: (id) => dispatch({type: actionTypes.HANDLER_REMOVE, payload: id}),
+    handleChange: (id) => dispatch({type: actionTypes.HANDLER_CHANGE, payload: id})
+  }
+
   return (
-    <eCommerceContext.Provider value={state, dispatch}>
+    <eCommerceContext.Provider value={value}>
       {children}
     </eCommerceContext.Provider>
   )
 }
 
+export function useEcommerce() {
+  const ctx = useContext(eCommerceContext)
+  if (!ctx) return null
+  return ctx
+}
 // Y context toma las variables del reducer y hacer return -> eCommerceContext 
