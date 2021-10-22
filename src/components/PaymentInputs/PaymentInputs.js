97235062db7e28ea9c13860b540cd4127b97d7e5
Haroon Ahmad <haroon334@gmail.com>
2021-10-22 11:27:25 +0200

    Before Merge

--- src/components/PaymentInputs/PaymentInputs.js
+++ src/components/PaymentInputs/PaymentInputs.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import { PaymentInputsWrapper, usePaymentInputs } from 'react-payment-inputs';
-import images from 'react-payment-inputs/images';
+// import React from "react";
+// //import { PaymentInputsWrapper, usePaymentInputs } from 'react-payment-inputs';
+// import images from "react-payment-inputs/images";
 
-export default function PaymentInputs() {
-  const {
-    wrapperProps,
-    getCardImageProps,
-    getCardNumberProps,
-    getExpiryDateProps,
-    getCVCProps
-  } = usePaymentInputs();
+// export default function PaymentInputs() {
+//   const {
+//     wrapperProps,
+//     getCardImageProps,
+//     getCardNumberProps,
+//     getExpiryDateProps,
+//     getCVCProps,
+//   } = usePaymentInputs();
 
-  return (
-    <PaymentInputsWrapper {...wrapperProps}>
-      <svg {...getCardImageProps({ images })} />
-      <input {...getCardNumberProps()} />
-      <input {...getExpiryDateProps()} />
-      <input {...getCVCProps()} />
-    </PaymentInputsWrapper>
-  );
-}
+//   return (
+//     <PaymentInputsWrapper {...wrapperProps}>
+//       <svg {...getCardImageProps({ images })} />
+//       <input {...getCardNumberProps()} />
+//       <input {...getExpiryDateProps()} />
+//       <input {...getCVCProps()} />
+//     </PaymentInputsWrapper>
+//   );
+// }
