3b75e6674528768f66464c8b7f3b50f666bcf0b8
andarbech <andarbech1.com>
2021-10-24 19:49:10 +0200

    finish resumen page

---
+++ src/components/CheckoutHeader/CheckoutHeader.js
@@ -0,0 +1,45 @@
+import { NavLink } from "react-router-dom"
+import { useUser } from "../../context/userContext/userContex"
+
+function CheckoutHeader (){
+     const {  userDataValidPage1,userDataValidPage2}= useUser()
+    return (
+        <>
+            <header>
+                <div className="justify-content-between checkout-header">
+                    <nav className="d-flex justify-content-around steps-nav">
+                        <NavLink exact activeClassName="is-active" to="/checkout/step-1">
+                            Information
+                        </NavLink>
+                        {userDataValidPage1? (
+                        <NavLink
+                            disabled={!userDataValidPage1}
+                            exact
+                            activeClassName="is-active"
+                            to="/checkout/step-2"
+                        >
+                            Shipping
+                        </NavLink>
+                        ) : (
+                        <span>Shipping</span>
+                        )}
+                        {userDataValidPage2 ?
+                        (
+                        <NavLink
+                            disabled={!userDataValidPage2}
+                            exact
+                            activeClassName="is-active"
+                            to="/checkout/step-3"
+                        >
+                            Payment
+                        </NavLink>
+                        ) : (
+                        <span>Payment</span>
+                        )
+                        }
+                    </nav>
+                </div>
+            </header>
+        </>
+    )
+} export default CheckoutHeader
--- src/pages/CheckoutStepThree/checkoutStepThree.js
+++ src/pages/CheckoutStepThree/checkoutStepThree.js
@@ -1,11 +1,17 @@
 import React from "react";
 import withLayout from "../../hoc/withLayout";
 import StepThreeForm from "../../components/StepThreeForm/index"
+import CheckoutHeader from "../../components/CheckoutHeader/CheckoutHeader";
 
 
 function CheckoutStepThree() {
     return (
         <>
+        <div>
+        <div>
+    <CheckoutHeader/>
+</div>
+        </div>
         <div>
             <StepThreeForm/>
         </div>
