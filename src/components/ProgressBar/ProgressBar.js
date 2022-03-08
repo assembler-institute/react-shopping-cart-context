3c417c92f245a407ccdb8e71321e08f7ac055466
Luis Molina <luismateo2416@gmail.com>
2022-03-08 15:19:27 +0100

    progress width

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,58 +5,40 @@ import { CheckoutContext } from "../../context/CheckoutContext";
 // styles
 import "./progressBar.scss";
 
+const STEPS = ["Account", "Shipping", "Payment", "Review"]
+
+
 export default function ProgressBar() {
     const { setStep, actualStep } = useContext(CheckoutContext)
+
+    const stepClassBtn = (stepNum) => {
+        if (actualStep > stepNum) return "mnoButton stepButton stepCompleted"
+        if (actualStep === stepNum) return "mnoButton stepButton stepActive"
+        return "mnoButton stepButton"
+    }
+
     return (
         <div className="progressBar mflex mrow mjustify-between">
             <div className="stepSecondaryLine ">
-                <div className="stepLine" />
+                <div className={`stepLine progress-${actualStep}`} />
             </div>
-            <div className="stepContainer mflex mcol malign-center  ">
-                <button
-                    type="button"
-                    onClick={() => { setStep(1) }}
-                    className={actualStep > 1 ? "mnoButton stepButton completed"
-                        : "mnoButton stepButton"}
-                >
-                    1
-                </button>
-                <p>Account</p>
-            </div>
-            <div className="stepContainer mflex mcol malign-center ">
-                <button
-                    type="button"
-                    onClick={() => { setStep(2) }}
-                    className={actualStep > 2 ? "mnoButton stepButton completed"
-                        : "mnoButton stepButton"}
-                >
-                    2
-                </button>
-                <p>Shipping</p>
-            </div>
-            <div className="stepContainer mflex mcol malign-center ">
-                <button
-                    type="button"
-                    onClick={() => { setStep(3) }}
-                    className={actualStep > 3 ? "mnoButton stepButton completed"
-                        : "mnoButton stepButton"}
-                >
-                    3
-                </button>
-                <p>Payment</p>
-            </div>
-            <div className="stepContainer mflex mcol malign-center ">
-                <button
-                    type="button"
-                    onClick={() => { setStep(4) }}
-                    className={actualStep >= 4 ? "mnoButton stepButton completed"
-                        : "mnoButton stepButton"}
-                >
-                    1
-                </button>
-                <p>Review</p>
-            </div>
+            {STEPS.map((step, index) => {
+                const stepNum = index + 1
+                return (
+                    <div key={step} className="stepContainer mflex mcol malign-center  " >
+                        <button
+                            type="button"
+                            disabled={actualStep < stepNum}
+                            onClick={() => { setStep(stepNum) }}
+                            className={stepClassBtn(stepNum)}
+                        >
+                            {stepNum}
+                        </button>
+                        <p>{step}</p>
+                    </div>
+                )
+            })}
 
-        </div>
+        </div >
     )
 }
