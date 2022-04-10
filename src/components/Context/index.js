6abc1abfdda77c3084bbdcaa081df17ef9355836
Josep Romero Ros <joseprrr@gmail.com>
2022-04-10 23:12:27 +0200

    updating

--- src/components/Context/index.js
+++ src/components/Context/index.js
@@ -1,1 +1,2 @@
-export { default } from "../../components/Context/UserContext"
+export { default } from "../../components/Context/UserContext";
+export { default } from "../../components/Context/reducer";
